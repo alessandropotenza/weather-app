024e262989b0fb00c8d9d716192f097b7b81e144
Alessandro Potenza <potenza1702@gmail.com>
2022-02-17 15:01:22 -0600

    Added weather app code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
+import SearchIcon from '@mui/icons-material/Search';
+import Clear from './assets/sunny.jpg';
+import Cloudy from './assets/cloudy.jpg';
+import Rain from './assets/rain.jpg';
+import Snow from './assets/snowing.jpg';
+import Haze from './assets/haze.jpg';
+import Thunderstorm from './assets/thunderstorm.jpg';
+import Default from './assets/default.jpg';
+
+const apiKey = '61f531d06489d307dfb36dc024855c88'; //used for github demonstration only
 
 function App() {
+  const [cityInput, setCityInput] = useState('');
+  const [locationInfo, setLocationInfo] = useState({});
+
+  const doFetch = () => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(result => setLocationInfo({
+      cityName: result.name,
+      temp: result.main.temp,
+      condition: result.weather[0].main,
+      description: result.weather[0].description,
+      country: result.sys.country
+    }));
+    setCityInput('');
+  };
+
+  const enterSearch = e => {
+    if(e.key === "Enter") {
+      doFetch();
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app' style= {
+          locationInfo.condition === 'Clear' ? {backgroundImage: `linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0)), url(${Clear})`}
+        : locationInfo.condition === 'Rain' ? {backgroundImage: `linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0)), url(${Rain})`}
+        : locationInfo.condition === 'Snow' ? {backgroundImage: `linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0)), url(${Snow})`}
+        : locationInfo.condition === 'Haze' ? {backgroundImage: `linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0)), url(${Haze})`}
+        : locationInfo.condition === 'Thunderstorm' ? {backgroundImage: `linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0)), url(${Thunderstorm})`}
+        : locationInfo.condition === 'Clouds' ? {backgroundImage: `linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0)), url(${Cloudy})`}
+        : {backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0)), url(${Default})`}
+      }>
+        {Object.keys(locationInfo).length === 0 ?
+          <>
+            <div class='welcome-message'>
+              <p>Find your local weather</p>
+            </div>
+          </>
+         :
+         <>
+          <div className='left-box'>
+            <p className='location'>{locationInfo.cityName}, {locationInfo.country}</p>
+            <p className='description'>{locationInfo.description}</p>
+          </div>
+          <div className='right-box'>
+            <p>{Math.round(locationInfo.temp)}°C</p>
+          </div>
+         </>
+        }
+        <div className='search-block'>
+          <input type='text' className='search-space' placeholder='Enter your city...' onKeyPress={enterSearch} value={cityInput} onChange={e => setCityInput(e.target.value)}/>
+          <SearchIcon className='search-button' onClick={doFetch} fontSize='large'/>
+        </div>
     </div>
   );
 }
